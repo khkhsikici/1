8dce1240f14e9d226bee4ba8423928e1a1593eb4
khkhsikici <khkh40369@gmail.com>
2024-08-26 16:05:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Dev by. ADSIZIM","Hexta 💜"];
 
 
 let currentIndex = 0;
